1217bed5391f7e9a322b8605392a96312eed6f4a
Ilyam123 <ilya12508@gmail.com>
2019-05-23 14:06:10 -0700

    Working Java connection class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,32 +7,31 @@ public class Main {
     public static Connection getConnection() throws SQLException {
 
         try {
-            Class.forName ("org.h2.Driver");
+            Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
         try {
             String url = "jdbc:h2:~/test";
-            String user = "USERNAME"; // CHANGE THIS
-            String password = "PASSWORD"; // CHANGE THIS
+            String user = "root"; // CHANGE THIS
+            String password = "Moka2003"; // CHANGE THIS
 
             // create a connection to the database
             Connection conn = DriverManager.getConnection(url, user, password);
             return conn;
         } catch (SQLException e) {
             System.out.println(e);
-        return null;
+            return null;
+        }
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static void main (String[] args) throws SQLException {
         Connection con = getConnection();
         Statement statement = con.createStatement();
 
-        statement.addBatch("CREATE TABLE IF NOT EXISTS Players(id INTEGER PRIMARY KEY AUTO_INCREMENT, team INTEGER, " +
-                "Player_Name TEXT, OutS DOUBLE, OutD DOUBLE, InS DOUBLE, InD DOUBLE, Reb DOUBLE)");
+        statement.execute("INSERT INTO Stats (speed, strength) VALUES (3, 3);");
 
-        statement.executeBatch();
-
+        con.close();
     }
 }
